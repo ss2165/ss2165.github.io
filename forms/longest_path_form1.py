e36c5cfb7614556c109850573deed4003af3e079
ss2165 <ss2165@cam.ac.uk>
2015-07-17 16:24:53 +0100

    Longest drag + paths
    Add velocity drag and path drawing to Longest Path form

--- forms/longest_path_form1.py
+++ forms/longest_path_form1.py
@@ -7,6 +7,44 @@ class Form1(Form1Template):
     #acceleration vector
     g = physics.vector3(0, -9.81)
     floor_height = 0.05
+    arrow_scale = 0.05
+    linewidth = 0.01
+    def txt_change (self, **event_args):
+        self.init_ball()
+
+    def canvas_mouse_move (self, x, y, **event_args):
+        # This method is called when the mouse cursor moves over this component
+        #record mouse pos
+        self.mouse.x = x/self.xu
+        self.mouse.y = (self.ch-y)/self.xu
+
+        #change text box value based on where mouse is
+        if self.mousedown:
+            x = ((self.mouse - self.ball.pos).x)
+            y = ((self.mouse - self.ball.pos).y)
+            ang = math.atan(y/x)
+            if 0<=ang<=math.pi/2:
+                self.txt_ang.text = "{0}".format(int(ang*180/math.pi))
+                self.ball.vel = physics.vector3(self.v*math.cos(ang), self.v*math.sin(ang))
+
+
+
+    def canvas_mouse_up (self, x, y, button, **event_args):
+        # This method is called when a mouse button is released on this component
+        self.mousedown = False
+
+    def canvas_mouse_down (self, x, y, button, **event_args):
+        # This method is called when a mouse button is pressed on this component
+        self.mouse.x = x/self.xu
+        self.mouse.y = (self.ch-y)/self.xu
+
+        #arrow detect
+        if (0.9*self.arrow_scale*self.ball.vel + self.ball.pos - self.mouse).mag()<= self.ball.radius/3:
+            self.mousedown= True
+
+    def btn_path_click (self, **event_args):
+        self.paths= []
+
     def btn_run_click (self, **event_args):
         # This method is called when the button is clicked
         if not self.running:
@@ -19,6 +57,9 @@ class Form1(Form1Template):
         else:
             self.running = False
             self.btn_run.text = "Run"
+
+        self.cur_path = []
+        self.paths.append(self.cur_path)
         #make parameters only editable while reset
         for box in self.param_boxes:
             box.enabled = self.reset
@@ -64,6 +105,8 @@ class Form1(Form1Template):
 
             else:
                 self.ball.move(dt)
+                if int(self.t/self.dt) %3 ==0:
+                    self.cur_path.append(self.ball.pos)
                 self.ball.vel = self.ball.vel + self.g*dt
                 self.path += dt*self.ball.vel.mag()
                 self.t += dt
@@ -90,6 +133,33 @@ class Form1(Form1Template):
         canvas.fill_style= "#3683f3"
         canvas.fill()
 
+        #arrow
+        if not self.running:
+            ball = self.ball
+            #velocity vector
+            canvas.translate(ball.pos.x, ball.pos.y)
+            if ball.vel.y>0:
+                canvas.rotate(ball.vel.phi())
+            else:
+                canvas.rotate(-ball.vel.phi())
+            draw.arrow(canvas, ball.vel.mag()*self.arrow_scale, 4*self.linewidth)
+            canvas.fill_style = "#49902a"
+            canvas.fill()
+            draw.reset2(canvas, xu)
+
+        #dashes
+        canvas.begin_path()
+        if not self.running:
+            for path in self.paths:
+                if len(path)>2:
+                    for i in range(len(path)-1):
+                        canvas.move_to(path[i].x, path[i].y)
+                        diff = path[i+1] - path[i]
+                        new  = path[i] + diff*0.8
+                        canvas.line_to(new.x, new.y)
+
+            canvas.line_width = 0.01
+            canvas.stroke()
     def init_ball(self):
         xsp = 0
         ysp = 0
@@ -124,7 +194,11 @@ class Form1(Form1Template):
         self.first = True
         self.dt = self.timer.interval
 
+        self.mousedown = False
+        self.mouse = physics.vector3(0,0,0)
+
         self.t = 0
+        self.paths = []
         #SET SCALE (pixels per m, or unit used in code)
         self.xu = 6
         self.v = 0
